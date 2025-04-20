6f0931c0bf5a02feddf4e61b06e6d3e0285c1029
xeusteerapat <xeus085@gmail.com>
2025-04-20 12:56:36 +0700

    🛠️ config style-loader

--- webpack.config.js
+++ webpack.config.js
@@ -2,32 +2,36 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.tsx',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/',
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
-  ],
-  devServer: {
-    historyApiFallback: true,
-    port: 3000,
-    hot: true,
-  },
+	entry: './src/index.tsx',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js',
+		publicPath: '/',
+	},
+	resolve: {
+		extensions: ['.tsx', '.ts', '.js'],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(ts|tsx)$/,
+				use: 'ts-loader',
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader'],
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './public/index.html',
+		}),
+	],
+	devServer: {
+		historyApiFallback: true,
+		port: 3000,
+		hot: true,
+	},
 };
